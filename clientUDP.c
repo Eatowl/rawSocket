afba2a674ff946c33b58bef1da406e965dde218d
Eatowl <csnelcov@gmail.com>
2017-03-16 10:47:54 +0700

    20 mess

--- clientUDP.c
+++ clientUDP.c
@@ -10,34 +10,31 @@
 #include <sys/un.h>
 
 #define PCKT_LEN 8192
-#define SIN_PORT 2020
-#define DIN_PORT 8080
 
 struct ipheader {
-	unsigned char      iph_ihl:5;
+	unsigned char      iph_ihl:4;
 	unsigned char      iph_ver:4;
 	unsigned char      iph_tos;
 	unsigned short int iph_len;
 	unsigned short int iph_ident;
-	unsigned char      iph_flag;
 	unsigned short int iph_offset;
 	unsigned char      iph_ttl;
 	unsigned char      iph_protocol;
 	unsigned short int iph_chksum;
 	unsigned int       iph_sourceip;
 	unsigned int       iph_destip;
-};
+}__attribute__((packed));
 
 struct udpheader {
 	unsigned short int udph_srcport;
 	unsigned short int udph_destport;
 	unsigned short int udph_len;
 	unsigned short int udph_chksum;
-};
+}__attribute__((packed));
 
 unsigned short csum(unsigned short *buf, int nwords) {
 	unsigned long sum;
-	for(sum = 0; nwords > 0; nwords--)
+	for (sum = 0; nwords > 0; nwords--)
 		sum += *buf++;
 		sum = (sum >> 16) + (sum & 0xffff);
 		sum += (sum >> 16);
@@ -59,23 +56,23 @@ int main() {
 	}
 	sin.sin_family = AF_INET;
 	din.sin_family = AF_INET;
-	sin.sin_port = htons(SIN_PORT);
-	din.sin_port = htons(DIN_PORT);
+	sin.sin_port = htons(2020);
+	din.sin_port = htons(8080);
 	sin.sin_addr.s_addr = inet_addr("127.0.0.3");
 	din.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	ip->iph_ihl = 5;
 	ip->iph_ver = 4;
-	ip->iph_tos = 16; // Low delay
+	ip->iph_tos = 32;
 	ip->iph_len = sizeof(struct ipheader) + sizeof(struct udpheader);
 	ip->iph_ident = htons(54321);
-	ip->iph_ttl = 64; // hops
-	ip->iph_protocol = 17; // UDP
+	ip->iph_ttl = 64;
+	ip->iph_protocol = 17;
 	ip->iph_sourceip = inet_addr("127.0.0.3");
 	ip->iph_destip = inet_addr("127.0.0.1");
 
-	udp->udph_srcport = htons(atoi(SIN_PORT));
-	udp->udph_destport = htons(atoi(DIN_PORT));
+	udp->udph_srcport = htons(2020);
+	udp->udph_destport = htons(8080);
 	udp->udph_len = htons(sizeof(struct udpheader));
 
 	ip->iph_chksum = csum((unsigned short *)buffer, sizeof(struct ipheader) + sizeof(struct udpheader));
